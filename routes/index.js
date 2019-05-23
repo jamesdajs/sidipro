00f0746e14e8c3cd5aae2f72eeca1b375523f318
Daniel Jaimes <james.rock.alt@gmail.com>
2019-05-23 17:21:34 -0400

    soap

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,14 @@
 var express = require('express');
-
+var XMLHttpRequest =require("xmlhttprequest").XMLHttpRequest
 var Usuario = require('../servicios/Usuario')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  Usuario.listarUsuarios()
-    .then(data => {
-      console.log(data)
 
-      res.render('index', { title: 'Express',data:data});
-    })
 });
 router.get('/usuarios',(req,res)=>{
-  Usuario.listarUsuarios()
-  .then(data=>{
-    res.json(data)
-  })
-  .catch(err=>{
-    console.log(err);
-    
-  })
+
 })
 router.get('/pagosnet',(req,res)=>{
   var sr =
